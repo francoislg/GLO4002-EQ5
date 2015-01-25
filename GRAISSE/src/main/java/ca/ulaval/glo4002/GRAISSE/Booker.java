35d2e5fa166bf0defc5874254cb56edb2e8512eb
MilknCookie <Alex.gagnon.01@gmail.com>
2015-01-25 16:22:17 -0500

    Booker

--- GRAISSE/src/main/java/ca/ulaval/glo4002/GRAISSE/Booker.java
+++ GRAISSE/src/main/java/ca/ulaval/glo4002/GRAISSE/Booker.java
@@ -1,5 +1,25 @@
 package ca.ulaval.glo4002.GRAISSE;
 
 public class Booker {
+	private Bookings bookings;
+	private BookingStrategy bookingStrategy;
+	private BookingStrategiesFactory bookingStrategiesFactory;
+	private Boardrooms boardrooms;
 
+	public Booker() {
+		bookings = new Bookings();
+		bookingStrategiesFactory = new BookingStrategiesFactory();
+		bookingStrategy = bookingStrategiesFactory
+				.setForBasicStrategy(boardrooms);
+
+		boardrooms = new Boardrooms();
+	}
+
+	public void assignBooking() {
+		bookingStrategy.assignBookings(bookings);
+	}
+
+	public void addBooking(Booking bookingToAdd) {
+		bookings.addBooking(bookingToAdd);
+	}
 }
