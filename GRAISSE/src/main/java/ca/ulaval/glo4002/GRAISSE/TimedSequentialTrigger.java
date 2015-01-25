569d6d73504172d08e965f03502faa59f257a0a0
Trylz <trylzengine@outlook.com>
2015-01-25 17:31:18 -0500

    class TimedSequentialTrigger

--- GRAISSE/src/main/java/ca/ulaval/glo4002/GRAISSE/TimedSequentialTrigger.java
+++ GRAISSE/src/main/java/ca/ulaval/glo4002/GRAISSE/TimedSequentialTrigger.java
@@ -1,5 +1,48 @@
 package ca.ulaval.glo4002.GRAISSE;
 
+
 public class TimedSequentialTrigger extends Trigger{
 
+	
+	private static final long DEFAULT_FREQUENCY_MINUTES = 10;
+	
+	private long timeFrequencyInMs;
+	public long lastActivationInMs;
+	
+	TimedSequentialTrigger()
+	{
+		lastActivationInMs=System.currentTimeMillis();
+		timeFrequencyInMs = DEFAULT_FREQUENCY_MINUTES*60000;
+	}
+	
+	public boolean checkActivation()
+	{
+		long actualTime = System.currentTimeMillis();
+		if( (actualTime - lastActivationInMs)>= timeFrequencyInMs )
+		{
+			lastActivationInMs=actualTime;
+			return true;
+			
+		}
+		return false;
+	}
+	
+	public void setFrequency(long frequency)
+	{
+		timeFrequencyInMs = frequency*60*1000;
+
+	}
+	
+
+	public long getFrequency()
+	{
+		return timeFrequencyInMs/60000;
+	}
+	
+	
+
+	
+
+	
+	
 }
--- GRAISSE/src/test/java/ca/ulaval/glo4002/GRAISSE/TimedSequentialTriggerTest.java
+++ GRAISSE/src/test/java/ca/ulaval/glo4002/GRAISSE/TimedSequentialTriggerTest.java
@@ -1,14 +1,60 @@
 package ca.ulaval.glo4002.GRAISSE;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 
 public class TimedSequentialTriggerTest {
 	
+
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void defaultFrequencyIsPositive() {
+		
+		TimedSequentialTrigger objectToTest = new TimedSequentialTrigger();
+		assertTrue(objectToTest.getFrequencyInMinutes()>0);
+
 	}
+	
+	@Test
+	public void lastActivationIsPositive() {
+		
+		TimedSequentialTrigger objectToTest = new TimedSequentialTrigger();
+		assertTrue(objectToTest.lastActivationInMs>0);
+
+	}
+	
+	@Test
+	public void isSetAndGetFrequencyWorking() {
+		
+		long aNumber = 5 ;
+		TimedSequentialTrigger objectToTest = new TimedSequentialTrigger();
+		objectToTest.setFrequency(aNumber,true);
+		
+		assertTrue(objectToTest.getFrequencyInMinutes()==aNumber);
+
+	}
+	
+
+	
+	@Test
+	public void triggerCanActivate() {
+		
+		long littleFrequencyInMs =  2;
+		
+		TimedSequentialTrigger objectToTest = new TimedSequentialTrigger();
+		objectToTest.setFrequency(littleFrequencyInMs,false);
+		
+		try{
+		    Thread.sleep(littleFrequencyInMs);
+		}catch(InterruptedException e){
+		    System.out.println("got interrupted!");
+		}
+
+		assertTrue(objectToTest.checkActivation());
+		
+	}
+	
+	
 }
