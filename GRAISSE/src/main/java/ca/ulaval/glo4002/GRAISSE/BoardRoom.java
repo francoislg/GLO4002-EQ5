edadcd6dae38e3611f3f32f24174e93e5a46ec12
MilknCookie <Alex.gagnon.01@gmail.com>
2015-01-25 16:21:55 -0500

    Boardroom

--- GRAISSE/src/main/java/ca/ulaval/glo4002/GRAISSE/BoardRoom.java
+++ GRAISSE/src/main/java/ca/ulaval/glo4002/GRAISSE/BoardRoom.java
@@ -1,14 +1,37 @@
 package ca.ulaval.glo4002.GRAISSE;
 
-public class BoardRoom {
-	int numberOfSeats;
+public class Boardroom {
+	private int numberOfSeats;
+	private String name;
+	private boolean available;
 
-	public BoardRoom(int numberOfSeats) {
+	public Boardroom(String name, int numberOfSeats) {
+		this.name = name;
 		this.numberOfSeats = numberOfSeats;
+		this.available = true;
 	}
 
-	public boolean verifyNumberOfSeats(int numberOfSeatsNeeded) {
-		return numberOfSeatsNeeded <= this.numberOfSeats;
+	private boolean isAvailable() {
+		return this.available;
 	}
 
+	public boolean isMyName(String name) {
+		return this.name == name;
+	}
+
+	public boolean verifyNumberOfSeats(Booking bookingToVerify) {
+		return bookingToVerify.verifyNumberOfSeats(numberOfSeats);
+	}
+
+	public boolean assign(Booking bookingToAssign) {
+		if (!verifyNumberOfSeats(bookingToAssign)) {
+			return false;
+		}
+		if (isAvailable()) {
+			bookingToAssign.assign();
+			available = false;
+			return true;
+		}
+		return false;
+	}
 }
