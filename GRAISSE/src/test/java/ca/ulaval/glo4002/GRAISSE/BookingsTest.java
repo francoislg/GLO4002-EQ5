4a3f43a0490d0ed8d1dea2f2f427b7995f29739a
MilknCookie <Alex.gagnon.01@gmail.com>
2015-01-27 19:09:53 -0500

    Correctif pour infinitytest
    remove extends testcase

--- GRAISSE/src/test/java/ca/ulaval/glo4002/GRAISSE/BookingsTest.java
+++ GRAISSE/src/test/java/ca/ulaval/glo4002/GRAISSE/BookingsTest.java
@@ -1,7 +1,8 @@
 package ca.ulaval.glo4002.GRAISSE;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
-import junit.framework.TestCase;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,7 +11,7 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
-public class BookingsTest extends TestCase {
+public class BookingsTest {
 
 	Bookings bookings;
 
