59d692c26a111f5de705f643f391f5547ddde6fc
MilknCookie <Alex.gagnon.01@gmail.com>
2015-01-28 20:17:53 -0500

    Boardrooms

--- GRAISSE/src/main/java/ca/ulaval/glo4002/GRAISSE/Boardrooms.java
+++ GRAISSE/src/main/java/ca/ulaval/glo4002/GRAISSE/Boardrooms.java
@@ -1,7 +1,10 @@
 package ca.ulaval.glo4002.GRAISSE;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Boardrooms {
 	private List<Boardroom> boardrooms = new ArrayList<Boardroom>();
@@ -36,7 +39,20 @@ public class Boardrooms {
 		return false;
 	}
 
-	public boolean assignToMaxSeatsBoardroom(Booking bookingToAssign) {
+	public boolean assignBookingToMinSeatsBoardroom(Booking bookingToAssign) {
+		Stream<Boardroom> boardroomsStream = getStreamSortByNumberOfSeats();
+		Iterator<Boardroom> boardroomIt = boardroomsStream.iterator();
+		while (boardroomIt.hasNext()) {
+			Boardroom boardroom = boardroomIt.next();
+			if (boardroom.assign(bookingToAssign)) {
+				return true;
+			}
+		}
 		return false;
+	}
+
+	private Stream<Boardroom> getStreamSortByNumberOfSeats() {
+		Comparator<Boardroom> byNumberOfSeats = (e1, e2) -> Integer.compare(e1.getNumberOfSeats(), e2.getNumberOfSeats());
+		return boardrooms.stream().sorted(byNumberOfSeats);
 	}
 }
