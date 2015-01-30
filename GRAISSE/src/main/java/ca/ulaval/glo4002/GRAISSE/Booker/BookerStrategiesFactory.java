5b1a1333ff29e89e19d4f97d54532d7417db6848
francoislg <francois_lg@hotmail.com>
2015-01-30 13:12:23 -0500

    Fix des changements avec les nouveaux packages

--- GRAISSE/src/main/java/ca/ulaval/glo4002/GRAISSE/Booker/BookerStrategiesFactory.java
+++ GRAISSE/src/main/java/ca/ulaval/glo4002/GRAISSE/Booker/BookerStrategiesFactory.java
@@ -3,6 +3,7 @@ package ca.ulaval.glo4002.GRAISSE.Booker;
 import ca.ulaval.glo4002.GRAISSE.Boardroom.BoardroomsStrategyBasic;
 import ca.ulaval.glo4002.GRAISSE.Boardroom.BoardroomsStrategyMaximise;
 import ca.ulaval.glo4002.GRAISSE.Booking.BookingsStrategyBasic;
+import ca.ulaval.glo4002.GRAISSE.Booking.BookingsStrategyPriority;
 
 public class BookerStrategiesFactory {
 
---
+++ GRAISSE/src/main/java/ca/ulaval/glo4002/GRAISSE/Booking/BookingsStrategyPriority.java
@@ -0,0 +1,16 @@
+package ca.ulaval.glo4002.GRAISSE.Booking;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+public class BookingsStrategyPriority extends BookingsStrategyBasic {
+	public BookingsStrategyPriority() {
+
+	}
+
+	public Collection<Booking> format(Collection<Booking> bookings) {
+		Comparator<Booking> byPriorityValue = (booking1, booking2) -> booking1.comparePriorityToBooking(booking2);
+		return bookings.stream().sorted(byPriorityValue).collect(Collectors.toList());
+	}
+}
