aa9ea5bfb73cfb06c1eb233e036e96f3fa6ade25
jpgiroux <holygrinder@gmail.com>
2015-02-07 12:47:13 -0500

    rename et implémentation du ThresholdTrigger

--- GRAISSE/src/main/java/ca/ulaval/glo4002/GRAISSE/TresholdTrigger.java
+++
@@ -1,9 +0,0 @@
-package ca.ulaval.glo4002.GRAISSE;
-
-public class TresholdTrigger extends Trigger{
-
-	public TresholdTrigger(Worker target) {
-		super(target);
-	}
-
-}
--- GRAISSE/src/test/java/ca/ulaval/glo4002/GRAISSE/TresholdTriggerTest.java
+++
@@ -1,6 +0,0 @@
-package ca.ulaval.glo4002.GRAISSE;
-
-
-public class TresholdTriggerTest {
-
-}
