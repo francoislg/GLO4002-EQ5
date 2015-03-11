091c1dd86ee285eeaf42f63e97246624b155c4ef
francoislg <francois_lg@hotmail.com>
2015-03-11 19:58:57 -0400

    Fin du déplacement des fichiers

--- GRAISSE/src/main/java/ca/ulaval/glo4002/GRAISSE/trigger/InvalidThresholdException.java
+++
@@ -1,6 +0,0 @@
-package ca.ulaval.glo4002.GRAISSE.trigger;
-
-public class InvalidThresholdException extends RuntimeException {
-
-	private static final long serialVersionUID = 3513286298739411091L;
-}
