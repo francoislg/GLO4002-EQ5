05eac0fb0551930bad734c41e04ebf3cefb590c4
jpgiroux <girouxjep@gmail.com>
2015-03-10 23:31:53 -0400

    refs #23 #25 #26 derniere modification suite au commentaire

--- GRAISSE/src/main/java/ca/ulaval/glo4002/GRAISSE/bookingExceptions/InvalidPriorityException.java
+++
@@ -1,6 +0,0 @@
-package ca.ulaval.glo4002.GRAISSE.bookingExceptions;
-
-public class InvalidPriorityException extends RuntimeException {
-
-	private static final long serialVersionUID = 811764802586795990L;
-}
