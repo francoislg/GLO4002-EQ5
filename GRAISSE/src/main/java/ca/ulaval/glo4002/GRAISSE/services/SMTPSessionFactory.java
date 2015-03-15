f19dca593e1d417ec0e83b216bbd59d2580a93a8
francoislg <francois_lg@hotmail.com>
2015-03-15 10:31:53 -0400

    Ajout de SMTPSessionFactory

---
+++ GRAISSE/src/main/java/ca/ulaval/glo4002/GRAISSE/services/SMTPSessionFactory.java
@@ -0,0 +1,29 @@
+package ca.ulaval.glo4002.GRAISSE.services;
+
+import java.util.Properties;
+
+import javax.mail.Session;
+
+public class SMTPSessionFactory {
+	private final String DEFAULT_PORT = "587";
+	
+	public Session create(String host, String user, String password){
+		return create(host, user, password, DEFAULT_PORT);
+	}
+	
+	public Session create(String host, String user, String password, String port) {
+		Properties properties = createPropertiesObject(host, user, password, port);
+		return Session.getInstance(properties);
+	}
+	
+	private Properties createPropertiesObject(String host, String user, String password, String port) {
+		Properties properties = new Properties();
+		properties.put("mail.smtp.starttls.enable", "true");
+		properties.put("mail.smtp.host", host);
+		properties.put("mail.smtp.user", user);
+		properties.put("mail.smtp.password", password);
+		properties.put("mail.smtp.port", port);
+		properties.put("mail.smtp.auth", "true");
+		return properties;
+	}
+}
