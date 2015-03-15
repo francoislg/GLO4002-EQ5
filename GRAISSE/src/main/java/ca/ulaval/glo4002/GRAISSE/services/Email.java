3410a5e30b3a1c8e2e702202c880e4ff93b0d8ef
francoislg <francois_lg@hotmail.com>
2015-03-15 14:23:47 -0400

    emailAdresse -> emailAddress

--- GRAISSE/src/main/java/ca/ulaval/glo4002/GRAISSE/services/Email.java
+++ GRAISSE/src/main/java/ca/ulaval/glo4002/GRAISSE/services/Email.java
@@ -6,17 +6,17 @@ import java.util.regex.Pattern;
 import ca.ulaval.glo4002.GRAISSE.services.exceptions.InvalidEmailException;
 
 public class Email {
-	private final String emailAdresse;
+	private final String emailAddress;
 	// Known regex for email validation
 	private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
 	public Email(String email) {
 		validateEmail(email);
-		this.emailAdresse = email;
+		this.emailAddress = email;
 	}
 
 	public String getValue() {
-		return emailAdresse;
+		return emailAddress;
 	}
 
 	private void validateEmail(String email) {
@@ -32,7 +32,7 @@ public class Email {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + emailAdresse.hashCode();
+		result = prime * result + emailAddress.hashCode();
 		return result;
 	}
 
@@ -45,10 +45,10 @@ public class Email {
 		if (getClass() != obj.getClass())
 			return false;
 		Email other = (Email) obj;
-		if (emailAdresse == null) {
-			if (other.emailAdresse != null)
+		if (emailAddress == null) {
+			if (other.emailAddress != null)
 				return false;
-		} else if (!emailAdresse.equals(other.emailAdresse))
+		} else if (!emailAddress.equals(other.emailAddress))
 			return false;
 		return true;
 	}
