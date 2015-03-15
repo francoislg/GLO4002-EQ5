dd46c13a34c922fdd11697967d5b559120d9e429
francoislg <francois_lg@hotmail.com>
2015-03-15 15:21:12 -0400

    Ajout d'un UserRepository bien simple

---
+++ GRAISSE/src/main/java/ca/ulaval/glo4002/GRAISSE/user/UserRepository.java
@@ -0,0 +1,8 @@
+package ca.ulaval.glo4002.GRAISSE.user;
+
+import ca.ulaval.glo4002.GRAISSE.services.Email;
+
+public interface UserRepository {
+	public User retrieve(Email email);
+	public void persist(User user);
+}
