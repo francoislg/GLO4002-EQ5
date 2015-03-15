d244d87aed4686c84b7d604b0a7f1152e62dd838
francoislg <francois_lg@hotmail.com>
2015-03-15 15:21:26 -0400

    Changé arraylist pour l'interface dans les repo

--- GRAISSE/src/main/java/ca/ulaval/glo4002/GRAISSE/persistence/BookingInMemoryRepository.java
+++ GRAISSE/src/main/java/ca/ulaval/glo4002/GRAISSE/persistence/BookingInMemoryRepository.java
@@ -2,13 +2,14 @@ package ca.ulaval.glo4002.GRAISSE.persistence;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import ca.ulaval.glo4002.GRAISSE.booking.Booking;
 import ca.ulaval.glo4002.GRAISSE.booking.BookingRepository;
 
 public class BookingInMemoryRepository implements BookingRepository{
 
-	private ArrayList<Booking> bookings;
+	private List<Booking> bookings;
 	
 	public BookingInMemoryRepository() {
 		bookings = new ArrayList<Booking>();
