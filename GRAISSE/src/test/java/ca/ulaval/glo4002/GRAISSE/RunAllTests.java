4c21cbebbf021fd9c29b64e2cdfd2ff783ffc8ef
Francois Lachance-Guillemette <francois.lguillemette@gmail.com>
2015-03-26 09:07:29 -0400

    Supprimé RunAllTests
    Ça causait la confusion et on s'est fait dire que c'est vraiment pas
    nécessaire.
    Si vos tests roulent pu, choisissez la config "run tests in package" ou
    quelque chose comme ça.

--- GRAISSE/src/test/java/ca/ulaval/glo4002/GRAISSE/RunAllTests.java
+++
@@ -1,9 +0,0 @@
-package ca.ulaval.glo4002.GRAISSE;
-
-import org.junit.extensions.cpsuite.ClasspathSuite;
-import org.junit.runner.RunWith;
-
-@RunWith(ClasspathSuite.class)
-public class RunAllTests {
-
-}
