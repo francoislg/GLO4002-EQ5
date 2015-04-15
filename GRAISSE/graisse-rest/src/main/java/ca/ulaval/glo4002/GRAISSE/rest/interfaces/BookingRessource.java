5eb43f49e92f15eb264766b577074cdb4d4bf092
Francois Lachance-Guillemette <francois_lg@hotmail.com>
2015-04-14 20:15:50 -0400

    BookingRessource mis à jour, c'est fonctionnel !

--- GRAISSE/graisse-rest/src/main/java/ca/ulaval/glo4002/GRAISSE/rest/interfaces/BookingRessource.java
+++ GRAISSE/graisse-rest/src/main/java/ca/ulaval/glo4002/GRAISSE/rest/interfaces/BookingRessource.java
@@ -1,8 +1,5 @@
 package ca.ulaval.glo4002.GRAISSE.rest.interfaces;
 
-import java.util.Collection;
-
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -11,20 +8,30 @@ import javax.ws.rs.core.MediaType;
 
 import ca.ulaval.glo4002.GRAISSE.core.booking.Booking;
 import ca.ulaval.glo4002.GRAISSE.core.booking.BookingRepository;
+import ca.ulaval.glo4002.GRAISSE.core.shared.Email;
+import ca.ulaval.glo4002.GRAISSE.persistence.BookingInMemoryRepository;
+import ca.ulaval.glo4002.GRAISSE.persistence.BookingNotFoundException;
+import ca.ulaval.glo4002.GRAISSE.rest.contexts.BookingRepositoryFiller;
+import ca.ulaval.glo4002.GRAISSE.rest.interfaces.form.RetrievedBookingResponse;
 
 @Path("/demandes")
-@Produces(MediaType.APPLICATION_JSON)
 public class BookingRessource {
 	private BookingRepository bookingRepository;
-	 
-    public BookingRessource(BookingRepository bookingRepository) {
-    	this.bookingRepository = bookingRepository;
-    }
- 
-    @GET
-    @Path("/{COURRIEL}/{NUMERO_DEMANDE}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Collection<Booking> validateCredentials(@PathParam("COURRIEL") String promoter, @PathParam("NUMERO_DEMANDE") String ID) {
-        return this.bookingRepository.retrieveAll();
-    }
+
+	public BookingRessource() {
+		this.bookingRepository = new BookingInMemoryRepository();
+		new BookingRepositoryFiller().fill(bookingRepository);
+	}
+
+	@GET
+	@Path("/{COURRIEL}/{NUMERO_DEMANDE}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public RetrievedBookingResponse getBooking(@PathParam("COURRIEL") String promoter, @PathParam("NUMERO_DEMANDE") String ID) {
+		try {
+			Booking foundBooking = bookingRepository.retrieveBooking(new Email(promoter), ID);
+			return new RetrievedBookingResponse(foundBooking);
+		} catch (BookingNotFoundException exception) {
+			throw new BookingNotFoundWebException("Il n'existe pas de demande \"" + ID + "\" pour l'organisateur \"" + promoter + "\"");
+		}
+	}
 }
