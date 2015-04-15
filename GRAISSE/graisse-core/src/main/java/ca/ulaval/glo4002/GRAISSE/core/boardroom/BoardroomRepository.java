043218c741b550d91817acda1de1bc3cc0e92ab0
Francois Lachance-Guillemette <francois_lg@hotmail.com>
2015-04-14 21:26:30 -0400

    Lancé un organize import :)

--- GRAISSE/graisse-core/src/main/java/ca/ulaval/glo4002/GRAISSE/core/boardroom/BoardroomRepository.java
+++ GRAISSE/graisse-core/src/main/java/ca/ulaval/glo4002/GRAISSE/core/boardroom/BoardroomRepository.java
@@ -1,8 +1,6 @@
 package ca.ulaval.glo4002.GRAISSE.core.boardroom;
 
 import java.util.Collection;
-import java.util.Comparator;
-import java.util.stream.Collectors;
 
 public interface BoardroomRepository {
 
@@ -11,6 +9,6 @@ public interface BoardroomRepository {
 	public Boardroom retrieve(String boardroomName);
 
 	public Collection<Boardroom> retrieveAll();
-	
-	public Collection<Boardroom>  retrieveBoardroomsOrderedByNumberOfSeats();
+
+	public Collection<Boardroom> retrieveBoardroomsOrderedByNumberOfSeats();
 }
