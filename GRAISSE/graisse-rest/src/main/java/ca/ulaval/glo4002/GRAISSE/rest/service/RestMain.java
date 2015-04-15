4b83dbf5c6779a3f10e8d3b5b113206a3661bd68
Francois Lachance-Guillemette <francois_lg@hotmail.com>
2015-04-14 20:16:23 -0400

    Update du RestMain

--- GRAISSE/graisse-rest/src/main/java/ca/ulaval/glo4002/GRAISSE/rest/service/RestMain.java
+++ GRAISSE/graisse-rest/src/main/java/ca/ulaval/glo4002/GRAISSE/rest/service/RestMain.java
@@ -6,27 +6,27 @@ import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
- 
+
 public class RestMain {
- 
-    public static void main(String[] args) throws Exception {
-        new RestMain().execute();
-    }
- 
-    public void execute() throws Exception {
-        int httpPort = 8080;
- 
-        Server server = new Server(httpPort);
-        ServletContextHandler servletContextHandler = new ServletContextHandler(server, "/");
-        configurerJersey(servletContextHandler);
-        server.start();
-        server.join();
-    }
- 
-    private void configurerJersey(ServletContextHandler servletContextHandler) {
-        ServletContainer container = new ServletContainer(new ResourceConfig().packages(
-                "ca.ulaval.glo4002.GRAISSE.interfaces.rest").register(JacksonFeature.class));
-        ServletHolder jerseyServletHolder = new ServletHolder(container);
-        servletContextHandler.addServlet(jerseyServletHolder, "/*");
-    }
+
+	public static void main(String[] args) throws Exception {
+		new RestMain().execute();
+	}
+
+	public void execute() throws Exception {
+		int httpPort = 8080;
+
+		Server server = new Server(httpPort);
+		ServletContextHandler servletContextHandler = new ServletContextHandler(server, "/");
+		configurerJersey(servletContextHandler);
+		server.start();
+		server.join();
+	}
+
+	private void configurerJersey(ServletContextHandler servletContextHandler) {
+		ServletContainer container = new ServletContainer(new ResourceConfig().packages("ca.ulaval.glo4002.GRAISSE.rest.interfaces").register(
+				JacksonFeature.class));
+		ServletHolder jerseyServletHolder = new ServletHolder(container);
+		servletContextHandler.addServlet(jerseyServletHolder, "/*");
+	}
 }
