d54f0f1dac62629b060f9da8751cc5646a5de39f
Francois Lachance-Guillemette <francois_lg@hotmail.com>
2015-04-14 21:26:00 -0400

    Il manquait un Equals+Hashcode, arg.

--- GRAISSE/graisse-rest/src/main/java/ca/ulaval/glo4002/GRAISSE/rest/interfaces/form/RetrievedBookingResponse.java
+++ GRAISSE/graisse-rest/src/main/java/ca/ulaval/glo4002/GRAISSE/rest/interfaces/form/RetrievedBookingResponse.java
@@ -20,4 +20,40 @@ public class RetrievedBookingResponse {
 		this.salleAssigne = salleAssigne;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((courrielOrganisateur == null) ? 0 : courrielOrganisateur.hashCode());
+		result = prime * result + nombrePersonnes;
+		result = prime * result + ((salleAssigne == null) ? 0 : salleAssigne.hashCode());
+		result = prime * result + ((statutDemande == null) ? 0 : statutDemande.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RetrievedBookingResponse other = (RetrievedBookingResponse) obj;
+		if (courrielOrganisateur == null) {
+			if (other.courrielOrganisateur != null)
+				return false;
+		} else if (!courrielOrganisateur.equals(other.courrielOrganisateur))
+			return false;
+		if (nombrePersonnes != other.nombrePersonnes)
+			return false;
+		if (salleAssigne == null) {
+			if (other.salleAssigne != null)
+				return false;
+		} else if (!salleAssigne.equals(other.salleAssigne))
+			return false;
+		if (statutDemande != other.statutDemande)
+			return false;
+		return true;
+	}
 }
