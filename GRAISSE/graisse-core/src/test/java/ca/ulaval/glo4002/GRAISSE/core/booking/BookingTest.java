eec91aa1d3c1de8616ef8b5322b45ebec2a068ce
MilknCookie <Alex.gagnon.01@gmail.com>
2015-04-16 10:04:01 -0400

    ajout de test ignore a faire

--- GRAISSE/graisse-core/src/test/java/ca/ulaval/glo4002/GRAISSE/core/booking/BookingTest.java
+++ GRAISSE/graisse-core/src/test/java/ca/ulaval/glo4002/GRAISSE/core/booking/BookingTest.java
@@ -38,6 +38,12 @@ public class BookingTest {
 		booking = new Booking(user, NUMBER_OF_SEATS_IN_BOOKING, PRIORITY_VALUE_OF_BOOKING);
 	}
 
+	@Ignore
+	@Test
+	public void whenBookingIsInstantiateWithoutPriorityTheBookingShouldHaveAMediumPriority() {
+
+	}
+
 	@Test
 	public void assignShouldAssignTheBooking() {
 		booking.assign();
