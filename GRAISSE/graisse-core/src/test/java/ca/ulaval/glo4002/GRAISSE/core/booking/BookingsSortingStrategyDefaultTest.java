9cfa194df1e6314a8127f59dceedcff244b2cd4f
Francois Lachance-Guillemette <francois_lg@hotmail.com>
2015-04-19 14:45:54 -0400

    Ajout de tests pour BookingSortingStrategyDefault

--- GRAISSE/graisse-core/src/test/java/ca/ulaval/glo4002/GRAISSE/core/booking/BookingsSortingStrategyDefaultTest.java
+++ GRAISSE/graisse-core/src/test/java/ca/ulaval/glo4002/GRAISSE/core/booking/BookingsSortingStrategyDefaultTest.java
@@ -1,16 +1,53 @@
 package ca.ulaval.glo4002.GRAISSE.core.booking;
 
-import org.junit.Ignore;
+import static org.junit.Assert.assertArrayEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BookingsSortingStrategyDefaultTest {
 
-	@Ignore
+	@Mock
+	private BookingRepository bookingRepository;
+
+	@Mock
+	private Booking booking;
+
+	private Collection<Booking> bookingsList = new ArrayList<Booking>();
+	private BookingsSortingStrategyDefault bookingSorting;
+
+	@Before
+	public void setUp() {
+		when(bookingRepository.retrieveAll()).thenReturn(bookingsList);
+		bookingSorting = new BookingsSortingStrategyDefault();
+	}
+
 	@Test
 	public void sortShouldCallretrieveAllOnTheBookingRepository() {
+		bookingSorting.sort(bookingRepository);
 
+		verify(bookingRepository).retrieveAll();
+	}
+
+	@Test
+	public void sortShouldReturnAllBookingsFromRepository() {
+		fillBookingList();
+
+		Collection<Booking> result = bookingSorting.sort(bookingRepository);
+
+		assertArrayEquals(bookingsList.toArray(), result.toArray());
+	}
+
+	private void fillBookingList() {
+		bookingsList.add(booking);
 	}
 }
